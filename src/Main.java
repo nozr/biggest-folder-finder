7456c090835ec04839c7eeef0f4888ec2b053215
Work <radjabovnazar@gmail.com>
2022-09-19 17:40:16 +0400

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 import java.io.File;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
 
+
         String folderPath = "C:/Users/Work/Downloads";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
 
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = (System.currentTimeMillis() - start)/1000;
+        System.out.println(duration + "seconds");
 
          }
 
