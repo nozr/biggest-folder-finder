71c9f1f40b56d5ba969a52456f175f7e2c3707e6
Work <radjabovnazar@gmail.com>
2022-09-20 13:55:20 +0400

    dev

--- src/FolderSizeCalculator.java
+++ src/FolderSizeCalculator.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.Node;
-
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,15 +5,18 @@ import java.util.concurrent.RecursiveTask;
 
 public class FolderSizeCalculator extends RecursiveTask<Long> {
 
-    private File folder;
-    public FolderSizeCalculator(File folder){
-        this.folder = folder;
+    private Node node;
+
+    public FolderSizeCalculator(Node node){
+        this.node = node;
     }
 
     @Override
     protected Long compute() {
-
+        File folder = node.getFolder();
         if (folder.isFile()){
+            long length = folder.length();
+            node.setSize(length);
             return folder.length();
         }
 
@@ -23,13 +24,16 @@ public class FolderSizeCalculator extends RecursiveTask<Long> {
         List<FolderSizeCalculator> subTasks = new LinkedList<>();
         File[] files = folder.listFiles();
         for (File file : files){
-            FolderSizeCalculator task = new FolderSizeCalculator(file);
+            Node child = new Node(file);
+            FolderSizeCalculator task = new FolderSizeCalculator(child);
             task.fork();
             subTasks.add(task);
+            node.addChild(child);
         }
         for(FolderSizeCalculator task : subTasks){
             sum += task.join();
         }
+        node.setSize(sum);
         return sum;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,19 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
-    private static char[]  sizeMultipliers = {'B', 'K', 'M', 'G', 'T' };
     public static void main(String[] args) {
-        System.out.println(getHumanReadableSize(240640));
-        System.exit(0);
-        System.out.println(getSizeFromHumanReadable("235K"));
-        System.exit(0);
         String folderPath = "C:/Users/Work/Downloads";
         File file = new File(folderPath);
+        Node root = new Node(file);
 
         long start = System.currentTimeMillis();
 
         FolderSizeCalculator calculator =
-                new FolderSizeCalculator(file);
+                new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
-        System.out.println(size);
+
+        System.out.println(root);
 
         long duration = (System.currentTimeMillis() - start)/1000;
         System.out.println(duration + "seconds");
@@ -39,47 +36,7 @@ public class Main {
     }
 
 
-    //TODO: 24B,234KB, 36MB, 34GB, 42TB
 
-    public static String getHumanReadableSize(long size){
-        for (int i = 0; i < sizeMultipliers.length; i++)
-        {
-            double value = size / Math.pow(1024, i);
-            if (value < 1024){
-                return Math.round(value) + "" +
-                        sizeMultipliers[i] +
-                        (i > 0 ? "b" : "");
-            }
-        }
-
-        return "Very big!";
-    }
-
-    public static long getSizeFromHumanReadable(String size){
-        HashMap<Character, Integer> char2multiplier = getMultiplier();
-
-        char sizeFactor = size
-                .replaceAll("[0-9\\s]+", "" )
-                .charAt(0);
-        int multiplier = char2multiplier.get(sizeFactor);
-        long length = multiplier * Long.valueOf(
-                size.replaceAll("[^0-9]", "")
-        );
-        return length;
-    }
-
-    private static HashMap<Character, Integer> getMultiplier(){
-        HashMap<Character, Integer> char2multiplier = new HashMap<>();
-        for (int i = 0; i < sizeMultipliers.length; i++){
-            char2multiplier.put(
-                    sizeMultipliers[i],
-                    (int)Math.pow(1024,i)
-            );
-        }
-        return char2multiplier;
-
-
-    }
 
 }
 
